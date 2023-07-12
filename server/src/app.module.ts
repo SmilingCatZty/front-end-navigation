9f8ef28a9591150161e86a4d06cca5c8c73cf544
ZhouTianyang <SmilingCat.tyz@gmail.com>
2023-07-12 09:54:48 +0800

    refactor: [server]结构优化

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DocModule } from './doc/doc.module';
+import { DocModule } from './modules/doc/doc.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
